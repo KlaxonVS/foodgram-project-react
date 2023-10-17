5669fc05d312ad2fc49eb64015e0961795a7344f
klaxonvs <mcklaxon@gmail.com>
2023-10-17 09:55:37 +0300

    fixes and scripts

--- frontend/docker-bash.sh
+++ frontend/docker-bash.sh
@@ -1,1 +1,7 @@
-docker build -t vorvorsky/foodgram_frontend:x86_64 .
+#!/bin/bash
+
+ver=$(cat docker_ver.txt)
+ver=$(echo "$ver + 0.01" | bc)
+echo $ver > docker_ver.txt
+
+docker build -t vorvorsky/foodgram_frontend:v${ver} .
