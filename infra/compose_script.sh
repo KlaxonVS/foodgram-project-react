50ca35cb64cb9b341ac727043e9536660755e32c
klaxonvs <mcklaxon@gmail.com>
2023-10-17 10:01:18 +0300

    fixes and scripts

--- infra/compose_script.sh
+++ infra/compose_script.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 if [ $1 = "update" ]; then
-    docker compose pull
     docker compose -f docker-compose-server-update.yml up -d
     echo "docker-compose-server-update.yml" > status.txt
 fi
 if [ $1 = "first" ]; then
-    docker compose pull
     docker compose -f docker-compose-server-first.yml up -d
     echo "docker-compose-server-first.yml" > status.txt
 fi
 if [ $1 = "local" ]; then
-    docker compose pull
     docker compose -f docker-compose-server-local.yml up -d
     echo "docker-compose-server-local.yml" > status.txt
 fi
